6b225145466052c0e2e2ba00d3dc0f0faec6a3ba
Naoko Kurata <kurata.naoko.jp@gmail.com>
2018-03-23 12:41:55 -0400

    updated records.py and __init__.py

--- records/records.py
+++ records/records.py
@@ -7,47 +7,48 @@ Class Records object for class assignment
 import requests
 import pandas as pd
 
+
 class Records:
 
     """
-    Class object for retreaving biodiversity data from GBIF  
-    """ 
+    Class object for retreaving biodiversity data from GBIF
+    """
 
     def __init__(self, q=None, interval=None):
         self.q = q
         self.interval = interval
         self.params = {
             "q": q, 
-            "year": str(self.interval)[1:11], 
+            "year": str(self.interval)[1:-1], 
             "basisOfRecord": "PRESERVED_SPECIMEN",
             "hasCoordinate": "true",
             "hasGeospatialIssue": "false",
             "country": "US",
-            "offset": "301",
-            "limit": "600"
+            "offset": "0",
+            "limit": "300"
         }   
         
         self.df = self._get_all_records()
 
     def _get_all_records(self):
         "iterate until end of records"
-        start = 0
-        data = []  #empty list all the results will be add.
+        data = []  # empty list all the results will be add.
               
         while 1:  # 1 equal true, keep running until it breaks
             # make request and store results
             res = requests.get(
-                url = "http://api.gbif.org/v1/occurrence/search?", 
-                params = self.params
+                url="http://api.gbif.org/v1/occurrence/search?", 
+                params=self.params
             )                                                                                               
             # increment counter
-            self.params["offset"] = str(int(self.params["offset"]) + 300)  #offset is starting point. limit is last numebr
+            self.params["offset"] = str(int(self.params["offset"]) + 300)  
+            # offset is starting point. limit is last numebr
         
             # concatenate data 
-            idata = res.json()  #format results as jSON and store in idata 
-            data += idata["results"]  #pull results and add it to data
+            idata = res.json()  # format results as jSON and store in idata 
+            data += idata["results"]  # pull results and add it to data
         
-            # stop when end of record is reached #npk categories for offset, limit,endOfRecords (true or false),count
+            # stop when end of record is reached 
             if idata["endOfRecords"]:
                 break
            
